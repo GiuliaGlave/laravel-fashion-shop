5e59e124a20fea83e216ac2e22335ad7dfe1d877
Simone Marino <simo.marino@outlook.it>
2023-04-17 19:59:02 +0200

    Home route on logo fixed

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,10 @@
 <nav class="navbar navbar-expand-lg bg-light mb-4">
   <div class="container d-flex align-items-center">
     <div class="d-flex align-items-end me-2">
-      <img src="{{ asset('img/logo.png') }}" alt="logo" class="me-2">
-      <h3>Shoes Shop</h3>
+      <a class="navbar-brand" href="{{ route('homepage')}}">
+        <img src="{{ asset('img/logo.png') }}" alt="logo" class="me-2">
+        <span>Shoe shop</span>
+      </a>
     </div>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
@@ -11,5 +13,5 @@
         </li>
       </ul>
     </div>  
-   
 </nav>
+
